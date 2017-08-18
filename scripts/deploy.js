8e7fa5c30f8e0b10efe0185c881b169dea1d08ee
Daniel Lo Nigro <daniel@dan.cx>
2017-08-17 20:54:38 -0700

    Add deployment script (#36)
    * Add deployment script
    
    * Exit code 1 everywhere

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.AWS_REGION || 'us-west-1'});
+
+let file;
+try {
+  file = fs.readFileSync(__dirname + '/../function.zip');
+} catch (err) {
+  console.error('Could not read function.zip! ' + err.message + '. Please ensure that you have built babel-bot.');
+  process.exit(1);
+}
+
+console.log('Deploying...');
+const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+lambda.updateFunctionCode({
+  FunctionName: 'BabelBot',
+  Publish: true,
+  ZipFile: file,
+}, function(err, data) {
+  if (err) {
+    console.error('Could not update: ' + err.message);
+    process.exit(1);
+  } else {
+    console.log('Successfully updated!');
+  }
+});
